7051cff3464fe19f8a2ca9f26b12043e000d724d
fpetrakov <jakephin2015@gmail.com>
2022-07-22 21:43:25 +0300

    save components folder path to temporary directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
 const { Command } = require("commander");
 const generateComponentFiles = require("./generateComponentFiles.js");
 
@@ -11,10 +14,25 @@ program
   .version("0.0.1");
 
 program
-  .argument("<path>", "path to folder with components")
   .argument("<name>", "name of the component")
-  .action((path, name) => {
-    generateComponentFiles(path, name);
+  .option("-p, --path <path>", "path to folder with components")
+  .action((name, options) => {
+    const settingsFilePath = os.tmpdir() + "/.grcrc.json";
+
+    if (!options.path && !fs.existsSync(settingsFilePath)) {
+      throw Error("specify path to components folder");
+    }
+
+    if (options.path && !fs.existsSync(settingsFilePath)) {
+      fs.writeFileSync(settingsFilePath, `{"path":"${options.path}"}`);
+    }
+
+    if (options.path && fs.existsSync(settingsFilePath)) {
+      generateComponentFiles(options.path, name);
+    } else {
+      const { path } = JSON.parse(fs.readFileSync(settingsFilePath));
+      generateComponentFiles(path, name);
+    }
   });
 
 program.parse();
